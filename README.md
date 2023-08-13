5c03a736e105e7575558e771d09f8464f206d4a1
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-08-14 00:34:39 +0200

    adding scripts to help testing and debuging the shell

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # simple shell
 A simple UNIX command interpreter in C.
+# Quick start and instructions
+
+### To Compile
+
+``` console
+    $ ./make_.py build
+```
+### To run
+
+``` console
+    $ ./make_.py run
+```
+
+### To look for something in the code base!
+
+``` console
+    $ ./loc.py "something"
+```
+### To debug using valgrind
+``` console
+    $ ./make_.py debug
+```
+
+### To run the shell by piping an input file!
+``` console
+    $ ./make_.py pipe input_file_name
+```
+Note: input_file_name needs to exist and contain a command in each line.
--- make_.py
+++ make_.py
@@ -1,14 +1,23 @@
 #!/bin/python3
 from os import system
 from sys import argv
+from pathlib import Path
+
 BINARY_NAME = "hsh"
 ARGC = len(argv)
 CURRENT_SCRIPT = argv[0]
 PROGRAM        = f"./{ BINARY_NAME }"
 BUILD_COMMAND  = f"gcc -Wall -g -Werror -Wextra -pedantic -std=gnu89 *.c -o { BINARY_NAME }"
 DEBUG_COMMAND  = f"valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all ./{ BINARY_NAME }"
-PIPE_COMMAND   = f"valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all cat input | ./{ BINARY_NAME }"
-BUILD_MODE, TEST_MODE, DUMP_MODE, RUN_MODE, DEBUG_MODE, PIPE_MODE = "BUILD", "TEST", "DUMP", "RUN", "DEBUG", "PIPE"
+BUILD_MODE, RUN_MODE, DEBUG_MODE, PIPE_MODE = "BUILD", "RUN", "DEBUG", "PIPE"
+
+def isvalidmode(mode: str) -> bool:
+    return (
+        mode == BUILD_MODE 
+        or mode == RUN_MODE
+        or mode == PIPE_MODE
+        or mode == DEBUG_MODE
+    )
 
 def push_all(xs, ys):    
     for i in ys:
@@ -19,8 +28,6 @@ def push_all(xs, ys):
 def create_ascii_lowercase(): return " ".join([chr(i) for i in range(ord('a'), ord('z') + 1)])
 def create_ascii_uppercase(): return " ".join([chr(i) for i in range(ord('A'), ord('Z') + 1)])
 def create_ascii_numbers():   return " ".join([chr(i) for i in range(ord('0'), ord('9') + 1)])
-
-TEST_INPUT = [create_ascii_numbers()]
 
 def build():
     print(f"[*] BUILDING USING: { BUILD_COMMAND }")
@@ -34,34 +41,28 @@ def debug():
     print(f"[*] Debugging with: { DEBUG_COMMAND }")
     system(DEBUG_COMMAND)
 
-def test():
-    for command in TEST_INPUT:
-        print(f"[!] EXECUTE: {command}")
-        system(f"echo \"{command}\" | { PROGRAM }")
-
-def dump():
-    with open("input", 'w+') as fp:
-        for command in TEST_INPUT:
-            fp.write(command)
-            fp.write('\n')
-    
-    print("[*] written commands in `input`")
-
 def pipe_():
-    print(f"[!] TESTING WITH A PIPE: { PIPE_COMMAND }")
-    system(PIPE_COMMAND)
+    if ARGC > 2:
+        fn = argv[2]
+        if not Path(fn).exists():
+            print("[?] failed to pipe, file was not provided does not exit.")
+            return
+        
+        with open(fn) as fp:
+            commands = fp.readlines()
+
+            for cmd in commands:
+                cmd_ = f"echo \"{ cmd }\" | ./{ BINARY_NAME }"
+                print(f"[!] TESTING WITH A PIPE: { cmd_ }")
+                system(cmd_)
+        return
+    
+    print("[?] failed to pipe, file was not provided as the third arg.")
 
 def execute_mode(MODE: str):
-    if MODE == TEST_MODE:
-        test()
-        return
 
     if MODE == BUILD_MODE:
         build()
-        return
-    
-    if MODE == DUMP_MODE:
-        dump()
         return
     
     if MODE == RUN_MODE:
@@ -77,18 +78,22 @@ def execute_mode(MODE: str):
         return
 
     print("Unknown mode: %s" % MODE)
-    print(f"implemented modes => { BUILD_MODE.lower() }, { TEST_MODE.lower() }, { DEBUG_MODE.lower() }, { RUN_MODE.lower() }")
+    print(f"implemented modes => { BUILD_MODE.lower() }, { DEBUG_MODE.lower() }, { RUN_MODE.lower() }")
     exit(1)
 
 def main():
     if ARGC < 2:
         print()
 
-        print(f"usage: { CURRENT_SCRIPT } test | build | debug")
+        print(f"usage: { CURRENT_SCRIPT } pipe | build | debug | run [option: pipe_file_name]")
         print(f"----------------------------------------------")
-        print(f"    test: run the compiled project.")
-        print(f"    build: build the project.")
-        print(f"    debug: debug the program using valgrind.")
+        print(f"    pipe  : pipe a file into stdin of the program.")
+        print(f"    build : build the project.")
+        print(f"    debug : debug the program using valgrind.")
+        print(f"    run   : run the program.")
+        print(f"\nNOTE: You can also combine commands.")
+        print(f"Example: {CURRENT_SCRIPT} build debug")
+        print(f"the command above builds then debugs the program")
         print()
 
         exit(1)
@@ -96,6 +101,7 @@ def main():
     MODES = [
         mode.upper().strip() 
         for mode in argv[1:]
+        if isvalidmode(mode.upper())
     ]
     
     for mode in MODES: execute_mode(mode)
