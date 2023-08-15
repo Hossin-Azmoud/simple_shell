bcaa913adab752e64dfb218b71af145611d3b37c
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-08-15 22:36:21 +0100

    path 2

--- _more_path.c
+++ _more_path.c
@@ -1,27 +1,37 @@
 #include "_simple_shell.h"
-
+/**
+* print_path - function that print the path
+*
+*/
 void  print_path(void)
 {
 	path_manager(PRINT_PATH, NULL, NULL);
 }
-
+/**
+* resolve_command_path - function that resolve the path
+* @old_path: the old path of the command
+* @res: the result of the command
+*/
 void resolve_command_path(char **old_path, int *res)
 {
 	path_manager(FIND_CMD, old_path, res);
 }
-
+/**
+* get_tokenized_path - function that get the token of the path
+* Return: ou tor null
+*/
 char **get_tokenized_path(void)
 {
 	char *path     = _get_env("PATH");
 	char **out;
-	
+
 	if (!path)
 	{
 		_puts("err: path not found.\n");
-		return NULL;
+		return (NULL);
 	}
 
 	out = split_by_delim(path, ":");
 	free(path);
-	return out;
+	return (out);
 }
