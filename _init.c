0d404ffbb4ffb6e234cebf28ef800826af43099e
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-08-15 21:49:26 +0100

    init

--- _init.c
+++ _init.c
@@ -1,21 +1,29 @@
 #include "_simple_shell.h"
-void init_environment()
+
+/**
+ * init_environment - function that initial
+ *		the environment
+ */
+void init_environment(void)
 {
 	struct sigaction sa;
 
 	/* Init a signal handler. */
 	sa.sa_handler = handle_signal;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-    sigaction(SIGINT, &sa, NULL);
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGINT, &sa, NULL);
 
 	init_builtins();
 	init_env_manager();
 	init_path_manager();
 	init_state_manager();
 }
-	
-void uinit_environment()
+
+/**
+ * uinit_environment - function uinitial of the env
+ */
+void uinit_environment(void)
 {
 	release_path();
 	release_env_();
