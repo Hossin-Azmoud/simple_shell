f5f154dbf05db39c9259daa515f465baa1af5e0e
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-08-15 22:53:01 +0100

    last path

--- _more_shell_stat.c
+++ _more_shell_stat.c
@@ -7,12 +7,18 @@ int get_status(void)
 {
 	return (shell_state_manager(GET_STATUS, 0));
 }
-
+/**
+* set_status - function that set the status in of index
+* @payload: the arguments
+*/
 void set_status(int payload)
 {
 	shell_state_manager(SET_STATUS, payload);
 }
-
+/**
+* init_state_manager - function that initial the state manage
+*
+*/
 void init_state_manager(void)
 {
 	shell_state_manager(INIT_STATE, 0);
