64d835f5709e9280ab5a8727447d3202dc6ddda7
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-08-16 17:42:38 +0100

    the signal

--- _signal.c
+++ _signal.c
@@ -1,5 +1,9 @@
 #include "_simple_shell.h"
-
+/**
+ * _sig_int - function that take the signal of a int
+ * @sig: the argument or the signal
+ * Return: internal_signal or 0
+ */
 int _sig_int(int sig)
 {
 	static int internal_sig = 0;
@@ -10,7 +14,11 @@ int _sig_int(int sig)
 	internal_sig = sig;
 	return (0);
 }
-
+/**
+ * handle_signal - function that handle the signal which
+ *			manage the function _sig_int
+ * @signal: the argument or the signal
+ */
 void handle_signal(int signal)
 {
 	_sig_int(signal);
